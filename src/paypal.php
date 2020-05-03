1a1765c56c471d9815b3c6d565bd05fd8c58c57f
ma867 <ma867@njit.edu>
2020-05-03 02:52:01 -0400

    added shopcart

--- src/paypal.php
+++ src/paypal.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+$products_in_cart = $_SESSION['selectedProducts'];
 $products = array
 (
     array(
@@ -29,40 +29,37 @@ $products = array
         'price' => 10.00
     ),
 );
-print_r($_SESSION['selectedProducts']);
 
-/*
+
+
+
 // For testing purposes set this to true, if set to true it will use paypal sandbox
 $testmode = true;
 $paypalurl = $testmode ? 'https://www.sandbox.paypal.com/cgi-bin/webscr' : 'https://www.paypal.com/cgi-bin/webscr';
 // If the user clicks the PayPal checkout button...
-if (isset($_POST['paypal']) && $_SESSION['selectedProducts'] && !empty($_SESSION['selectedProducts'])) {
+if (isset($_POST['paypal']) && $products_in_cart && !empty($products_in_cart)) {
     // Variables we need to pass to paypal
     // Make sure you have a business account and set the "business" variable to your paypal business account email
     $data = array(
-        'cmd'            => '_cart',
+        'cmd'			=> '_cart',
         'upload'        => '1',
-        'lc'            => 'EN',
-        'business'         => 'payments@yourwebsite.com',
-        'cancel_return'    => 'https://yourwebsite.com/index.php?page=cart',
-        'notify_url'    => 'https://yourwebsite.com/index.php?page=cart&ipn_listener=paypal',
-        'currency_code'    => 'USD',
+        'lc'			=> 'EN',
+        'business' 		=> 'payments@yourwebsite.com',
+        'cancel_return'	=> 'https://yourwebsite.com/index.php?page=cart',
+        'notify_url'	=> 'https://yourwebsite.com/index.php?page=cart&ipn_listener=paypal',
+        'currency_code'	=> 'USD',
         'return'        => 'https://yourwebsite.com/index.php?page=placeorder'
     );
     // Add all the products that are in the shopping cart to the data array variable
     for ($i = 0; $i < count($products); $i++) {
         $data['item_number_' . ($i+1)] = $products[$i]['id'];
         $data['item_name_' . ($i+1)] = $products[$i]['name'];
-        $data['quantity_' . ($i+1)] = $_SESSION['selectedProducts'][$products[$i]['id']];
+        $data['quantity_' . ($i+1)] = $products_in_cart[$products[$i]['id']];
         $data['amount_' . ($i+1)] = $products[$i]['price'];
     }
-
     print_r($data);
-
-
-
     // Send the user to the paypal checkout screen
-   // header('location:' . $paypalurl . '?' . http_build_query($data));
+  //  header('location:' . $paypalurl . '?' . http_build_query($data));
     // End the script don't need to execute anything else
     exit;
 }
@@ -134,4 +131,3 @@ if (isset($_GET['ipn_listener']) && $_GET['ipn_listener'] == 'paypal') {
     exit;
 
 }
-*/
